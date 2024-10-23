edef0daabf3bdf44650e7d85110f20d30d25c0ae
Atsuyko <132264349+Atsuyko@users.noreply.github.com>
2024-10-23 12:17:48 +0200

    Mod fixtures + add HTPPS .env + Add code coverage

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,6 +15,7 @@ class AppFixtures extends Fixture
 {
     private $faker;
     private $adminCounter;
+    private $employeeCounter;
     private $userCounter;
     private $orderCounter;
     private $eventCounter;
@@ -28,75 +29,51 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $this->createOffer('Offre 1', 0, 1, $manager);
-        $this->createOffer('Offre 2', 5, 2, $manager);
-        $this->createOffer('Offre 3', 10, 4, $manager);
+        $this->createOffer('Solo', 0, 1, 'upload_608_phpC979.tmp.png', $manager);
+        $this->createOffer('Duo', 5, 2, 'upload_992_phpFA45.tmp.png', $manager);
+        $this->createOffer('Famille', 10, 4, 'upload_496_php35E8.tmp.png', $manager);
 
         $offerSolo = $this->getReference('offer1');
         $offerDuo = $this->getReference('offer2');
         $offerFam = $this->getReference('offer3');
 
-        $this->createAdmin('admin@test.com', 'Maxime', 'Admin', $manager);
+        $this->createAdmin('admin@jo.com', 'Admin', 'Fictif', $manager);
+        $this->createEmployee('employee@jo.com', 'Employee', 'Fictif', $manager);
+        $this->createUser('user@jo.com', 'User', 'Fictif', $manager);
 
-        /* Create 10 Users */
-        for ($i = 1; $i <= 10; $i++) {
-            $this->createUser($this->faker->email(), $this->faker->firstname(), $this->faker->lastname(), $manager);
-        }
-
-        /* Create 1 Order for each User (without admin) */
-        for ($i = 1; $i <= 10; $i++) {
-            $this->createOrder($this->getReference('user' . $i), $manager);
-        }
-
-        /* Create 10 Events */
-        for ($i = 1; $i <= 10; $i++) {
-            $this->createEvent('Epreuve ' . $i, 'Lieu de l\'épreuve ' . $i, new \DateTime('2025-' . mt_rand(1, 12) . '-' . mt_rand(1, 30)), new \DateTime(mt_rand(9, 12) . ':00:00'), new \DateTime(mt_rand(13, 17) . ':00:00'), mt_rand(50, 200), $offerSolo, $offerDuo, $offerFam, $manager);
-        }
-
-        /* Create 25 Tickets */
-        for ($i = 1; $i <= 25; $i++) {
-            $this->createTicket($this->getReference('event' . mt_rand(1, 10)), $this->getReference('offer' . mt_rand(1, 3)), $this->getReference('order' . mt_rand(1, 10)), $manager);
-        }
-
-        /* Update order's prices */
-        for ($i = 1; $i <= 10; $i++) {
-            $order = $this->getReference('order' . $i);
-            $totalOrderPrice = $order->getPrice();
-
-            for ($j = 1; $j <= 25; $j++) {
-                $ticket = $this->getReference('ticket' . $j);
-
-                if ($ticket->getOrder()->getId() === $order->getId()) {
-                    $totalOrderPrice += $ticket->getPrice();
-                }
-            }
-
-            $order->setPrice($totalOrderPrice)
-                ->setUpdatedAt(new \DateTimeImmutable());
-
-            $manager->persist($order);
-        }
+        $this->createEvent('Basket ', 'Terrain 1',  '2024-11-01', '10:00:00', '12:00:00', 100, 'upload_203_phpA59.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Athlétisme', 'Stade de France', '2024-11-16', '14:00:00', '18:00:00', 150, 'upload_167_phpA83B.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Natation', 'Aquaboulevard', '2024-12-05', '15:00:00', '19:00:00', 120, 'upload_889_phpA346.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Cyclisme', 'Vélodrome SQY', '2024-12-24', '09:00:00', '11:30:00', 80, 'upload_853_php2E9F.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Boxe', 'Ring 452', '2025-01-09', '12:00:00', '17:00:00', 70, 'upload_984_phpD83E.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Equitation', 'Château de Versailles', '2025-02-05', '13:00:00', '16:00:00', 90, 'upload_599_php7CFA.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Escalade', 'Mur 18', '2025-02-12', '17:00:00', '19:00:00', 50, 'upload_609_php45D9.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Escrime', 'Piste 8', '2024-12-13', '08:00:00', '11:00:00', 70, 'upload_573_phpBF60.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Gymnastique', 'Gymnase 7', '2024-11-02', '09:00:00', '17:00:00', 130, 'upload_727_php3C51.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
+        $this->createEvent('Tennis de table', 'Gymnase 56', '2024-11-16', '12:00:00', '15:00:00', 40, 'upload_284_phpB4BE.tmp.png', $offerSolo, $offerDuo, $offerFam, $manager);
 
         $manager->flush();
     }
 
     /**
-     * Create an offer with fixtures
+     * Create an offer
      *
      * @param string $name
      * @param integer $discount
      * @param integer $nbPeople
+     * @param string $picture
      * @param ObjectManager $manager
      * @return void
      */
-    public function createOffer(string $name, int $discount, int $nbPeople, ObjectManager $manager)
+    public function createOffer(string $name, int $discount, int $nbPeople, string $picture, ObjectManager $manager)
     {
         $offer = new Offer();
         $offer->setName($name)
             ->setDiscount($discount)
-            ->setNbPeople($nbPeople);
+            ->setNbPeople($nbPeople)
+            ->setPicture($picture);
 
-        /* Create a reference for event to make relation between entity with fixtures*/
+        /* Create a reference for event to make relation between entity */
         $this->addReference('offer' . $this->offerCounter + 1, $offer);
         $this->offerCounter++;
 
@@ -106,7 +83,7 @@ class AppFixtures extends Fixture
     }
 
     /**
-     * Create an admin with fixtures
+     * Create an admin
      *
      * @param string $email
      * @param string $firstname
@@ -121,9 +98,10 @@ class AppFixtures extends Fixture
             ->setFirstname($firstname)
             ->setLastname($lastname)
             ->setPlainPassword('password')
-            ->setRoles(["ROLE_ADMIN"]);
+            ->setRoles(["ROLE_ADMIN"])
+            ->setVerified(true);
 
-        /* Create a reference for admin to make relation between entity with fixtures*/
+        /* Create a reference for admin to make relation between entity */
         $this->addReference('admin' . $this->adminCounter + 1, $admin);
         $this->adminCounter++;
 
@@ -133,7 +111,35 @@ class AppFixtures extends Fixture
     }
 
     /**
-     * Create an user with fixtures
+     * Create an admin
+     *
+     * @param string $email
+     * @param string $firstname
+     * @param string $lastname
+     * @param ObjectManager $manager
+     * @return void
+     */
+    public function createEmployee(string $email, string $firstname, string $lastname, ObjectManager $manager)
+    {
+        $employee = new User();
+        $employee->setEmail($email)
+            ->setFirstname($firstname)
+            ->setLastname($lastname)
+            ->setPlainPassword('password')
+            ->setRoles(["ROLE_EMPLOYEE"])
+            ->setVerified(true);
+
+        /* Create a reference for employee to make relation between entity */
+        $this->addReference('employee' . $this->employeeCounter + 1, $employee);
+        $this->employeeCounter++;
+
+        $manager->persist($employee);
+
+        return $employee;
+    }
+
+    /**
+     * Create an user
      *
      * @param string $email
      * @param string $firstname
@@ -147,9 +153,10 @@ class AppFixtures extends Fixture
         $user->setEmail($email)
             ->setFirstname($firstname)
             ->setLastname($lastname)
-            ->setPlainPassword('password');
+            ->setPlainPassword('password')
+            ->setVerified(true);
 
-        /* Create a reference for user to make relation between entity with fixtures*/
+        /* Create a reference for user to make relation between entity */
         $this->addReference('user' . $this->userCounter + 1, $user);
         $this->userCounter++;
 
@@ -159,85 +166,41 @@ class AppFixtures extends Fixture
     }
 
     /**
-     * Create an event with fixtures
+     * Create an event
      *
      * @param string $name
      * @param string $place
-     * @param \DateTime $date
-     * @param \DateTime $startTime
-     * @param \DateTime $endTime
+     * @param string $date
+     * @param string $startTime
+     * @param string $endTime
      * @param integer $price
+     * @param string $picture
      * @param Offer $solo
      * @param Offer $duo
      * @param Offer $fam
      * @param ObjectManager $manager
      * @return void
      */
-    public function createEvent(string $name, string $place, \DateTime $date, \DateTime $startTime, \DateTime $endTime, int $price, Offer $solo, Offer $duo, Offer $fam, ObjectManager $manager)
+    public function createEvent(string $name, string $place, string $date, string $startTime, string $endTime, int $price, string $picture, Offer $solo, Offer $duo, Offer $fam, ObjectManager $manager)
     {
         $event = new Event();
         $event->setName($name)
             ->setPlace($place)
-            ->setDate($date)
-            ->setStartTime($startTime)
-            ->setEndTime($endTime)
+            ->setDate(new \DateTime($date))
+            ->setStartTime(new \DateTime($startTime))
+            ->setEndTime(new \DateTime($endTime))
             ->setPrice($price)
+            ->setPicture($picture)
             ->addOffer($solo)
             ->addOffer($duo)
             ->addOffer($fam);
 
-        /* Create a reference for event to make relation between entity with fixtures*/
+        /* Create a reference for event to make relation between entity */
         $this->addReference('event' . $this->eventCounter + 1, $event);
         $this->eventCounter++;
 
         $manager->persist($event);
 
         return $event;
-    }
-
-    /**
-     * Create an order with fixtures
-     *
-     * @param User $user
-     * @param ObjectManager $manager
-     * @return void
-     */
-    public function createOrder(User $user, ObjectManager $manager)
-    {
-        $order = new Order();
-        $order->setPrice(0)
-            ->setUser($user);
-
-        /* Create a reference for order to make relation between entity with fixtures*/
-        $this->addReference('order' . $this->orderCounter + 1, $order);
-        $this->orderCounter++;
-
-        $manager->persist($order);
-    }
-
-    /**
-     * Create a ticket with fixtures
-     *
-     * @param Event $event
-     * @param Offer $offer
-     * @param Order $order
-     * @param integer $price
-     * @param ObjectManager $manager
-     * @return void
-     */
-    public function createTicket(Event $event, Offer $offer, Order $order, ObjectManager $manager)
-    {
-        $ticket = new Ticket();
-        $ticket->setEvent($event)
-            ->setOffer($offer)
-            ->setOrder($order)
-            ->setPaid(0);
-
-
-        /* Create a reference for ticket to make relation between entity with fixtures*/
-        $this->addReference('ticket' . $this->ticketCounter + 1, $ticket);
-        $this->ticketCounter++;
-
-        $manager->persist($ticket);
     }
 }
